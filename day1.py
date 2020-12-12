44ff2465b40c5c03325a837bb78cfe12b4bfd48b
fudgebiscuit <ollie.zzephos@gmail.com>
2020-12-12 09:03:15 +0000

    removed comments

--- day1.py
+++ day1.py
@@ -11,12 +11,12 @@ def PartOne(target_num, input_file):
     try:
         num_1 = target_number//2
         possible_numbers = {target_number-x for x in list_of_numbers if x<=num_1} & {x for x in list_of_numbers if x>num_1}
-        return [[target_number-x, x] for x in possible_numbers]
+        return [[target_number-x, x] for x in possible_numbers][0]
     except:
         return 'Error: Solution could not be found'
 
 print('----- Part one -----')
 target_number = 2020
-result = PartOne(target_number, list_of_numbers)[0]
+result = PartOne(target_number, list_of_numbers)
 print('Two entries that sum to {} are {} and {}'.format(target_number, result[0], result[1]))
 print('Output number is {}'.format(result[0]*result[1]))
