b2ae7a4131c9ca5302eb46cc88aabe107e85da0d
fudgebiscuit <ollie.zzephos@gmail.com>
2020-12-12 09:01:17 +0000

    Changed code to make it from O(n^2) to O(n)

--- day1.py
+++ day1.py
@@ -1,17 +1,3 @@
-# import numpy as np
-# import pandas as pd
-
-# numbers = [2, 4, 6, 10]
-# target_number = 8
-
-# for i, num_1 in enumerate(numbers[:1]):
-#     num_2 = target_number - num_1
-#     if num_2 in numbers[i+1:]:
-#         print("Solution Found: {} and {}".format(num_1, num_2))
-#         break
-#     else:
-#         print("No solutions found")
-
 def FileCheck(filename):
     try:
         with open(filename) as f:
@@ -21,12 +7,16 @@ def FileCheck(filename):
 
 list_of_numbers = FileCheck('input-files/day1.txt')
 
+def PartOne(target_num, input_file):
+    try:
+        num_1 = target_number//2
+        possible_numbers = {target_number-x for x in list_of_numbers if x<=num_1} & {x for x in list_of_numbers if x>num_1}
+        return [[target_number-x, x] for x in possible_numbers]
+    except:
+        return 'Error: Solution could not be found'
 
+print('----- Part one -----')
 target_number = 2020
-for i, num_1 in enumerate(list_of_numbers):
-    num_2 = target_number - num_1
-    if num_2 in list_of_numbers[i+1:]:
-        print('Two entries that sum to {} are {} and {}'.format(target_number, num_1, num_2))
-        print('Output number is {}'.format(num_1*num_2))
-        break
-
+result = PartOne(target_number, list_of_numbers)[0]
+print('Two entries that sum to {} are {} and {}'.format(target_number, result[0], result[1]))
+print('Output number is {}'.format(result[0]*result[1]))
